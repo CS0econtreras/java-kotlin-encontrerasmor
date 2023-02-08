907ef7a78a3092bf022ff9e3cac8eade2431f28f
Ezequiel Contreras <ezeq317@gmail.com>
2023-02-08 13:26:39 -0700

    Class Test

--- classTest/app/src/main/java/com/example/classtest/account.java
+++ classTest/app/src/main/java/com/example/classtest/account.java
@@ -1,9 +1,17 @@
 package com.example.classtest;
 
 public class account {
-    String name;
-    int age;
+    private String name;
+    private int age;
+    account(int age, String name){
+        this.age = age;
+        this.name = name;
+    }
+    account(){
+        this(23,"Ezequiel");
+    }
     public static void main(String[] args){
+
         account a = new account();
         a.setName("Ezequiel");
         a.setAge(23);
@@ -12,10 +20,18 @@ public class account {
         a.showDetails();
     }
     public void setName(String name){
+        if(name == null){
+            throw new IllegalArgumentException("Name cannot be null");
+        }
         this.name = name;
     }
     public void setAge(int age){
-        this.age = age;
+        if(age < 123){
+            this.age = age;
+        }
+        else {
+            throw new IllegalArgumentException("Person can't be that old!");
+        }
     }
     public String getName(){
         return this.name;
@@ -25,6 +41,6 @@ public class account {
     }
     public void showDetails(){
         System.out.println(getAge() + ", " + getName());
-}
+    }
 
 }
--- classTest/app/src/test/java/com/example/classtest/accountTest.java
+++ classTest/app/src/test/java/com/example/classtest/accountTest.java
@@ -2,21 +2,36 @@ package com.example.classtest;
 
 import junit.framework.TestCase;
 
-public class accountTest extends TestCase {
+import org.junit.Test;
 
-    public void testMain() {
-    }
+public class accountTest extends TestCase {
 
     public void testTestSetName() {
     }
-
+    @Test
     public void testSetAge() {
+        account Account = new account();
+        Account.setAge(23);
+        assertEquals(23, Account.getAge());
+    }
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetAgeFail() {
+        account Account = new account();
+        Account.setAge(124);
+        assertEquals(124, Account.getAge());
     }
 
-    public void testTestGetName() {
+    public void testTestGetNameOk() {
+        account Account = new account();
+        Account.setName("Ezeuiel");
+        assertEquals("Ezequiel", Account.getName());
     }
 
-    public void testGetAge() {
+
+    public void testGetAgeFail() {
+        account Account = new account();
+        Account.setName(null);
+        assertEquals("Ezequiel", Account.getName());
     }
 
     public void testShowDetails() {
